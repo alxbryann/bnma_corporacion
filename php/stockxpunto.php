d8e8998fb61ffc166bb0856ec335505bc2b5a5eb
alxbryann <bryanalexanderbogota@gmail.com>
2024-04-26 20:37:21 -0500

    testing

--- php/stockxpunto.php
+++ php/stockxpunto.php
@@ -28,10 +28,7 @@ function Lista_Inventario()
     <link rel='stylesheet' type='text/css' href='/css/estilosMenu.css'>
     <link rel='preconnect' href='https://fonts.googleapis.com'>
     <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
-    <link href='https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap'
-        rel='stylesheet'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap' rel='stylesheet'>
     <header>
         <img src='/img/logo_blanco.png'>
         <h1>CORPORACION BNMA</h1>
@@ -39,43 +36,37 @@ function Lista_Inventario()
     <div id="h1Container">
         <h1>Inventario General</h1>
     </div>
-
+    
     <table>
-        <table class="table-danger">
-            <td class="table-danger">Empresa</td>
-            <td class="table-danger">Nombre</td>
-            <td class="table-danger">Stock</td>
-        </thead>
+        <tr>
+            <td>Empresa</td><td>Nombre</td><td>Stock</td>
         </tr>
-        <?php
-        if ($result = $mysqli->query($sql)) {
-            $row = 0;
-            while ($row = $result->fetch_assoc()) {
-                ?>
-                <tr class="table-danger">
-                    <?php
-                    if ($row["nitlocal"] == "901724534-7" && $row["nrosucursal"] == "1") {
-                        $row["nitlocal"] = "Drinks 1";
-                    }
-                    if ($row["nitlocal"] == "901724534-7" && $row["nrosucursal"] == "2") {
-                        $row["nitlocal"] = "Drinks 2";
-                    }
-                    if ($row["nitlocal"] == "901331637-1") {
-                        $row["nitlocal"] = "Gran Central";
-                    }
-                    ?>
-                    <td width="1%"><?= $row["nitlocal"] ?></td>
-                    <td width="5%"><?= $row["Nombre"] ?></td>
-                    <td width="5%"><?= $row["Stock"] ?></td>
-                </tr>
+    <?php
+    if ($result = $mysqli->query($sql)) {
+        $row = 0;
+        while ($row = $result->fetch_assoc()) {
+            ?>
+            <tr>
                 <?php
-                $id = $id + 1;
-            }
-        } ?>
+                if($row["nitlocal"] == "901724534-7" && $row["nrosucursal"] == "1") {
+                    $row["nitlocal"] = "Drinks 1";
+                }
+                if($row["nitlocal"] == "901724534-7" && $row["nrosucursal"] == "2") {
+                    $row["nitlocal"] = "Drinks 2";
+                }
+                if($row["nitlocal"] == "901331637-1") {
+                    $row["nitlocal"] = "Gran Central";
+                }
+                ?>
+                <td width="1%"><?= $row["nitlocal"] ?></td>
+                <td width="5%"><?= $row["Nombre"] ?></td>
+                <td width="5%"><?= $row["Stock"] ?></td>
+            </tr>
+            <?php
+            $id = $id + 1;
+        }
+    }?>
     </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
     <?php
 }
 ?>
