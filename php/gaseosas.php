5be34014c2579abf565d8d97ca4e26910c39abf1
alexbryann <bryanalexanderbogota@gmail.com>
2024-06-13 06:59:22 -0500

    correct index, testing gaseosas.php

---
+++ php/gaseosas.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./img/logo.png">
+    <link rel="stylesheet" href="./css/estilosIndex.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Gaseosas</title>
+</head>
+
+<body>
+    <header>
+        <img src="/img/logo_blanco.png">
+        <h1>CORPORACION BNMA</h1>
+    </header>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Coca cola 1.5</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Precio: 40000</li>
+            <li class="list-group-item">Stock: 20</li>
+        </ul>
+    </div>
+
+</body>
+
+</html>
