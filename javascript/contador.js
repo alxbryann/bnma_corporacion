662ec3e51b4d8c3d78d97b9ca16941f03bda27e4
alexbryann <bryanalexanderbogota@gmail.com>
2024-07-03 10:23:37 -0500

    Update contador.js

--- javascript/contador.js
+++ javascript/contador.js
@@ -1,5 +1,6 @@
-let numeroaSumar;
 const numeroElement = document.getElementById('numeros');
+var table = document.getElementById('tabla').getElementsByTagName('tbody')[0];
+let numeroaSumar;
 let viajes = parseInt(numeroElement.textContent, 10);
 
 function agregarViaje() {
@@ -19,15 +20,73 @@ function eliminarViaje() {
     } 
 }
 
+function agregarEntrada() {
+    var inputProducto = document.getElementById('modal-producto');
+    var inputNovedad = document.getElementById('modal-novedad');
+    var newRow = table.insertRow();
+    var productoCell = newRow.insertCell(0);
+    var cantidadCell = newRow.insertCell(1);
+    var novedadCell = newRow.insertCell(2);
+    var eliminarCell = newRow.insertCell(3);
+    productoCell.textContent = inputProducto.value;
+    cantidadCell.textContent = viajes;
+    novedadCell.textContent = inputNovedad.value;
+    numeroElement.textContent = "000";
+    viajes = 0;
+    numeroaSumar = 0;
+    hideModal();
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const inputElement = document.getElementById('input-numero');
     const numeroElement = document.getElementById('numeros');
 
-    // Detectar cuando el usuario ingrese un número en el campo de entrada
     inputElement.addEventListener('input', () => {
-        const nuevoNumero = parseInt(inputElement.value, 10); // Obtener el valor del input
+        const nuevoNumero = parseInt(inputElement.value, 10);
         if (!isNaN(nuevoNumero)) {
             numeroaSumar = nuevoNumero;
         }
     });
 });
+
+
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("openModalBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+function showModal() {
+    modal.classList.remove("hide");
+    modal.classList.add("show");
+    modal.style.display = "block";
+}
+
+
+function hideModal() {
+    modal.classList.remove("show");
+    modal.classList.add("hide");
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 500); 
+}
+
+
+btn.onclick = function() {
+    showModal();
+}
+
+
+span.onclick = function() {
+    hideModal();
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        hideModal();
+    }
+}
