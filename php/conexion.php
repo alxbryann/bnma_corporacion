3d9bd7fe418e23aceb99c604f0aa2e852368015a
alxbryann <bryanalexanderbogota@gmail.com>
2024-10-14 18:45:18 -0500

    change host

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli("82.197.80.210", "u482925761_alex2680", "Milo2680", "u482925761_bnmaweb");
+$mysqli = new mysqli("82.197.82.18", "u482925761_alex2680", "Milo2680", "u482925761_bnmaweb");
 if (mysqli_connect_errno()) {
 	echo 'Conexion Fallida : ', mysqli_connect_error();
 	exit();
