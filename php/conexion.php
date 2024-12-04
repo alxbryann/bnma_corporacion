1559ea8487764e851a449b27401655930ad1e7cc
Bryan <89673562+alxbryann@users.noreply.github.com>
2024-12-03 19:41:49 -0500

    restarting project

--- php/conexion.php
+++ php/conexion.php
@@ -2,9 +2,9 @@
 
 $mysqli = new mysqli("82.197.82.18", "u482925761_alex2680", "Milo2680", "u482925761_bnmaweb");
 if (mysqli_connect_errno()) {
-	echo 'Conexion Fallida : ', mysqli_connect_error();
+	echo 'Failed to connect', mysqli_connect_error();
 	exit();
 } else {
-
+	echo 'Successfully Connected';
 }
 ?>
